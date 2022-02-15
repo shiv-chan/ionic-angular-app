0339a13b7e761988be4036df4601c42623a6253a
shiv-chan <kshibuya31@gmail.com>
2022-02-15 15:27:26 -0800

    structure the master list page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { SigninComponent } from './signin/signin.component';
+import { MasterListComponent } from './master-list/master-list.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/signin', pathMatch: 'full' },
   { path: 'signin', component: SigninComponent },
+  { path: 'master-list', component: MasterListComponent },
 ];
 
 @NgModule({
