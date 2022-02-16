be80ca04ef349b547cb9d0709e6f43d9f5cf94a8
shiv-chan <kshibuya31@gmail.com>
2022-02-15 23:18:27 -0800

    add the function to fetch all picture from the external api and display it in view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
@@ -12,7 +13,12 @@ import { MasterListComponent } from './master-list/master-list.component';
 @NgModule({
   declarations: [AppComponent, SigninComponent, MasterListComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    HttpClientModule,
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
---
+++ src/app/pictures.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders,
+} from '@angular/common/http';
+import { environment } from '../environments/environment';
+import moment from 'moment';
+import { Pictures } from './pictures';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PicturesService {
+  constructor(private http: HttpClient) {}
+
+  // get all picture of 30 days prior today
+  private startDate = moment().subtract(30, 'days').format('YYYY-MM-DD');
+  private endDate = moment().format('YYYY-MM-DD');
+  // eslint-disable-next-line max-len
+  private picturesUrl = `https://api.nasa.gov/planetary/apod?api_key=${environment.apiKey}&start_date=${this.startDate}&end_date=${this.endDate}`;
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      console.error('An error occured:', error.error);
+    } else {
+      console.error(
+        `Backend returned code ${error.status}, body was: `,
+        error.error
+      );
+    }
+    return throwError('Something bad happened; please try again later.');
+  }
+
+  getImages(): Observable<any> {
+    return this.http
+      .get<Pictures>(this.picturesUrl)
+      .pipe(catchError(this.handleError));
+  }
+}
